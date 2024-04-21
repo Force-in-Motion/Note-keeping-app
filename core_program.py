2fb36aa48681a9e9e0e1c4748903124b2e0a48b9
Force-in-Motion <canzdao@gmail.com>
2024-04-21 18:20:07 +0300

    changes GUI

--- core_program.py
+++ core_program.py
@@ -1,6 +1,8 @@
 import input_data_user
 import modul_upper_level
 import output_data_user
+
+
 def run_program():
     output_data_user.print_output_greetings()
     input_data_user.input_name_note()
