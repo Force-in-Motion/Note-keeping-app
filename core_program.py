0ac4ab096163da94a979e44f23dd7bead54acdb3
Force-in-Motion <canzdao@gmail.com>
2024-04-21 18:00:31 +0300

    create Note app

---
+++ core_program.py
@@ -0,0 +1,20 @@
+import input_data_user
+import modul_upper_level
+import output_data_user
+def run_program():
+    output_data_user.print_output_greetings()
+    input_data_user.input_name_note()
+    # starts_main_loop()
+
+
+
+
+
+
+# def starts_main_loop():
+#     """
+#     Запускает главный жизненный цикл программы
+#     :return:
+#     """
+#     while True:
+#         if
---
+++ entry_point.py
@@ -0,0 +1,4 @@
+import core_program
+
+if __name__ == '__main__':
+    core_program.run_program()
