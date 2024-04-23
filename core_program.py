e494bbb89dfcc41d530aeffdb100c27d06d1543c
Force-in-Motion <canzdao@gmail.com>
2024-04-23 16:47:11 +0300

    develop

--- core_program.py
+++ core_program.py
@@ -1,16 +1,12 @@
 
 import modul_upper_level
 import GUI
+import save_data_user
 
+def run_program(data_set_notes):
+    GUI.print_output_menu()
 
-# def run_program():
-#     GUI.print_output_menu()
-#     GUI.input_data('Введите наименование заметки >> ')
-#     # starts_main_loop()
-
-
-
-
+    starts_main_loop(data_set_notes)
 
 
 def starts_main_loop():
@@ -20,13 +16,17 @@ def starts_main_loop():
     """
     while True:
         input_data = GUI.input_data('Введите команду >> ')
-        if input_data == 'stop':
-            return
-        if input_data == 'create':
-            data_set_notes = modul_upper_level.create_note()
-            # GUI.output_data(GUI.output_data_message['empty_note'])
-        if input_data == 'all notes':
-            print(data_set_notes)
+        if input_data != '':
+            if input_data == 'stop':
+                return
 
-
-
+            if input_data == 'create':
+                data_set_notes = save_data_user.create_matrix_data()
+                if not data_set_notes:
+                    GUI.output_data(GUI.output_data_message['err_save'])
+            if input_data == 'all notes':
+                print(data_set_notes)
+            else:
+                GUI.output_data(GUI.output_data_message['err_input'])
+        else:
+            GUI.output_data(GUI.output_data_message['empty_note'])
